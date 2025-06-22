b101751c5f631ff2eedf2a96174039f3697e02c7
Quoc Phong <DQPdak@users.noreply.github.com>
2025-06-22 16:45:53 +0700

    upload_1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const routes = require('./routes');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-
-dotenv.config()   
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8081;
 
-// Connect to DB    
-mongoose.connect(`mongodb+srv://ShopUTH:${process.env.MONGO_DB}@minhphung.exhsh77.mongodb.net/?retryWrites=true&w=majority&appName=MinhPhung`)
-    .then(() => console.log('Connected DB!'))
-    .catch(() => console.log('Connect failure!'))
+// Connect to DB
+mongoose
+  .connect(
+    `mongodb+srv://ShopUTH:${process.env.MONGO_DB}@minhphung.exhsh77.mongodb.net/?retryWrites=true&w=majority&appName=MinhPhung`
+  )
+  .then(() => console.log("Connected DB!"))
+  .catch(() => console.log("Connect failure!"));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -25,5 +27,5 @@ app.use(cors());
 routes(app);
 
 app.listen(PORT, () => {
-    console.log(`Server is running in port: ${PORT}`)
-})
+  console.log(`Server is running in port: ${PORT}`);
+});
