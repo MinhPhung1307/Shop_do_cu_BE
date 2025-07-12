9f254d62dac845b128aab448c34123e230d29f09
Nguyen Minh Phung <nguyenminhphung1307@gmail.com>
2025-07-12 08:19:16 +0700

    update avatar

--- src/index.js
+++ src/index.js
@@ -22,10 +22,11 @@ mongoose
 
 
 app.use("/uploads", express.static("uploads"));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 routes(app);
 
